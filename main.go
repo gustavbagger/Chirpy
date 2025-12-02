8ca40847bb98a7399ea5f343dd06c9217eeed0e4
gustavbagger <gustavbagger@me.com>
2025-12-02 11:21:19 +1100

    Special handler for server status

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 )
 
 func main() {
-	servemux := http.NewServeMux()
+	//Set up the defaults for serving files via urls like /app/foo
+	mux := http.NewServeMux()
+	file_server := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", file_server))
 
-	servemux.Handle("/", http.FileServer(http.Dir(".")))
+	/*
+		A special handler which can be called via /healthz
+		This is not a file but code that runs in response to a request
+		Examples include api usage, server status etc.
+	*/
+	handler := func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
+	mux.HandleFunc("/healthz", handler)
 
+	port := "8080"
 	server := http.Server{
-		Handler: servemux,
-		Addr:    ":8080",
+		Handler: mux,
+		Addr:    ":" + port,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
